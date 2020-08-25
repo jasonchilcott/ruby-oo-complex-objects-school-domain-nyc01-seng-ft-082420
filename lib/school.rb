0f1ce98ab3432776315020c511c2599b99974926
jasonchilcott <jasonchilcottcode@gmail.com>
2020-08-25 19:42:59 -0400

    created school class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end    
+
+    attr_accessor :name, :roster
+
+    def add_student(student, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end    
+        @roster[grade] << student
+    end    
+
+    def grade(gr)
+        @roster[gr]
+    end    
+
+    def sort
+        sorted = {}
+        @roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end   
+        sorted
+    end    
+
+
+end    
